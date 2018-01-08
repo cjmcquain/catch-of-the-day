9698cd75f4e092e9463e7d000b8e3fda367ae996
Charles McQuain <charlesmcquain@gmail.com>
2018-01-07 20:11:03 -0800

    Integrated Firebase Realtime database

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyAbffBSyBnBhth4bjGmZ4dwlZFR4D87ge0",
+    authDomain: "catch-of-the-day-charles.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-charles.firebaseio.com",
+});
+
+export default base;
